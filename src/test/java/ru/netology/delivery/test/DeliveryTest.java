26bf7726deff68569477404227c988e5ac988e26
Zosha <posyamyka@gmail.com>
2021-09-05 11:33:36 +0300

    после доработки

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -4,12 +4,12 @@ import lombok.var;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
 import static com.codeborne.selenide.Condition.exactText;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
 
 public class DeliveryTest {
 
@@ -28,15 +28,19 @@ public class DeliveryTest {
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
 
         $("[data-test-id=city] input").setValue(validUser.getCity());
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.SPACE);
         $("[placeholder='Дата встречи']").setValue(firstMeetingDate);
         $("[name='name']").setValue(validUser.getName());
         $("[name='phone']").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id=success-notification] .notification__title").shouldHave(exactText("Успешно!"));
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на  " + firstMeetingDate));
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.SPACE);
         $("[placeholder='Дата встречи']").setValue(secondMeetingDate);
         $$("button").find(exactText("Запланировать")).click();
         $$("button").find(exactText("Перепланировать")).click();
-        $("[data-test-id=success-notification] .notification__title").shouldHave(exactText("Успешно!"));
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на  " + secondMeetingDate));
     }
 }
